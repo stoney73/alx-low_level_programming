635acece85e67bdb40b80caeda50e0b0b05c38e4
stoney73 <hemstoneotieno3@gmail.com>
2022-12-12 06:22:09 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
-
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 
 }
