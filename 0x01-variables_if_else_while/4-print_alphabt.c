56b3dff8995ee49194a876d963c8c50bb882aed6
stoney73 <hemstoneotieno3@gmail.com>
2022-12-12 23:36:39 -0800

    task 4, task 5, task 6, task 7, task 8, task
    9

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,26 +2,24 @@
 #include <stdlib.h>
 
 /**
- * main - returns alphabets both lower and upper cases
+ * main - returns alphabets both except e and q
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
-int ch = 'a';
-int CH = 'A';
+char ch = 'a';
 
 while (ch <= 'z')
 {
+if ((ch != 'e') && (ch != 'q'))
+{
 putchar(ch);
+}
 ch++;
 }
-while (CH <= 'Z')
-{
-putchar(CH);
-CH++;
-}
-
 putchar('\n');
-return (0) 
+return (0);
 }
